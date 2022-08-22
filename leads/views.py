ff497d765d3c180d802bcd86f106fe34b6f66fba
Philippe Heurtaux <philippe.heurtaux@decathlon.com>
2022-08-22 22:13:02 +0200

    Structuring urls

--- leads/views.py
+++ leads/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Lead
 
 def home_page(request):
-    return HttpResponse('Hello world')
+    leads = Lead.objects.all(),
+    
+    context = {
+        'leads': leads
+    }
+    return render(request,"second_page.html",context)
