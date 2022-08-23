cc9ee17dee25b6254caa4c4f713f1cbb88774b2c
Philippe Heurtaux <philippe.heurtaux@decathlon.com>
2022-08-23 11:16:44 +0200

    Lead list and detail views

--- djcrm/urls.py
+++ djcrm/urls.py
@@ -16,8 +16,6 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
-from leads.views import home_page
-
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('leads/', include('leads.urls', namespace='leads')),
