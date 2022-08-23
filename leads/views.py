dadcf55203f395baaf13c950281646441240a9ce
Philippe Heurtaux <philippe.heurtaux@decathlon.com>
2022-08-23 21:17:20 +0200

    Lead creation form

--- leads/views.py
+++ leads/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
+from re import M
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
-from .models import Lead
+from .models import Lead, Agent
+from .forms import LeadForm
 
 def lead_list(request):
     leads = Lead.objects.all()
@@ -16,3 +18,26 @@ def lead_detail(request, pk):
         "lead": lead
     }
     return render(request, "lead_detail.html", context=context)
+
+def lead_create(request):
+    form = LeadForm()
+    if request.method == "POST":
+        form = LeadForm(request.POST)
+        if form.is_valid():
+            print(form.cleaned_data)
+            first_name = form.cleaned_data['first_name']
+            last_name = form.cleaned_data['last_name']
+            age = form.cleaned_data['age']
+            agent = Agent.objects.first()
+            Lead.objects.create(
+                first_name=first_name,
+                last_name=last_name,
+                age=age,
+                agent=agent
+            )
+            return redirect("/leads")
+             
+    context = {
+        "form": LeadForm()
+    }
+    return render(request, "lead_create.html", context)
